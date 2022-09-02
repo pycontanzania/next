87f65d508f1325a50b990594f80445211dbca19e
Albert <ishengomaalbert@gmail.com>
2022-09-02 12:13:40 +0300

    PWA enabled

--- next.config.js
+++ next.config.js
@@ -11,5 +11,11 @@ const nextConfig = {
     return config;
   },
 };
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true
+});
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
--- pages/404.js
+++ pages/404.js
@@ -8,12 +8,12 @@ function NotFound(props) {
       <div className="m-auto text-center">
 
         <div className="relative w-32 h-24 mx-auto mb-8">
-          <Image priority src={LogoImage} layout="fill" objectFit="contain" />
+          <Image priority src={LogoImage} alt="pycon-logo" layout="fill" objectFit="contain" />
         </div>
 
         <h1 className="text-green-600 text-xl font-bold">404</h1>
         <h2 className="text-gray-900 dark:text-gray-300 text-4xl md:text-6xl font-semibold md:font-bold my-4">Page Not Found</h2>
-        <p className="text-gray-600 dark:text-gray-400 w-4/5 md:w-full text-lg mx-auto my-4">Sorry, we couldn't find the page your looking for.</p>
+        <p className="text-gray-600 dark:text-gray-400 w-4/5 md:w-full text-lg mx-auto my-4">Sorry, we couldn&apos;t find the page your looking for.</p>
 
         <div className="relative w-32 h-24 mx-auto">
           <Image priority src={LogoImage} layout="fill" objectFit="contain" alt="PyCon Logo"/>
