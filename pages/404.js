9060af21afaebe3aada3061ce5a491341c7739be
Albert <ishengomaalbert@gmail.com>
2022-09-02 08:42:41 +0300

    Created favicons and web manifest file

--- pages/404.js
+++ pages/404.js
@@ -6,7 +6,7 @@ function NotFound(props) {
   return (
     <section className="h-screen flex">
       <div className="m-auto text-center">
-<<<<<<< HEAD
+
         <div className="relative w-32 h-24 mx-auto mb-8">
           <Image priority src={LogoImage} layout="fill" objectFit="contain" />
         </div>
@@ -14,7 +14,7 @@ function NotFound(props) {
         <h1 className="text-green-600 text-xl font-bold">404</h1>
         <h2 className="text-gray-900 dark:text-gray-300 text-4xl md:text-6xl font-semibold md:font-bold my-4">Page Not Found</h2>
         <p className="text-gray-600 dark:text-gray-400 w-4/5 md:w-full text-lg mx-auto my-4">Sorry, we couldn't find the page your looking for.</p>
-=======
+
         <div className="relative w-32 h-24 mx-auto">
           <Image priority src={LogoImage} layout="fill" objectFit="contain" alt="PyCon Logo"/>
         </div>
@@ -22,7 +22,7 @@ function NotFound(props) {
         <h1 className="text-green-600 text-xl font-bold">404</h1>
         <h2 className="text-4xl font-semibold my-4">Page Not Found</h2>
         <p className="w-4/5 mx-auto my-4">Sorry, we couldn&apos;t find the page your looking for.</p>
->>>>>>> a3a14a4b4f8398ac2220f45969c472d4892341b7
+
         <Link href="/">
           <a className="text-green-600">
             Go back home{" "}
