8c5ad53b3e4775052e4bb66b36f86b69a2531e00
Albert <ishengomaalbert@gmail.com>
2022-09-02 14:52:04 +0300

    Added robots.txt & sitemap fixes #11

---
+++ next-sitemap.config.js
@@ -0,0 +1,19 @@
+const siteUrl = "https://next-pycon.vercel.app/";
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl,
+    generateIndexSitemap: false,
+    exclude: ["/404"],
+    generateRobotsTxt: true,
+    robotsTxtOptions: {
+        policies: [{ userAgent: "*", disallow: ["/404"]},
+                   {  userAgent: "*", allow: "/" },
+        ],
+        additionalSitemaps: [
+            `${siteUrl}sitemap.xml`,
+            //FOR DYNAMIC PAGES
+            // `${siteUrl}/server-sitemap.xml`,
+          ]
+    },
+  }
--- next.config.js
+++ next.config.js
@@ -11,11 +11,11 @@ const nextConfig = {
     return config;
   },
 };
-const withPWA = require('next-pwa')({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
   register: true,
-  skipWaiting: true
+  skipWaiting: true,
 });
 
 module.exports = withPWA(nextConfig);
