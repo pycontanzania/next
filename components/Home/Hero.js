9975f8dd2be4250c6b83f633129706d9b3475731
Albert <ishengomaalbert@gmail.com>
2022-09-12 11:54:01 +0300

    Updated OpenGraph & Twitter Metatags for HomePage

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -2,27 +2,8 @@ import React from "react";
 
 function Hero(props) {
   return (
-    <section className="grid grid-cols-2">
-      <div>
-        <h1 className="uppercase text-green-600 text-xl font-semibold ">
-          KARIBU PYCON TANZANIA
-        </h1>
-        <h2 className=" text-7xl font-bold text-zinc-700">
-          The <span className=" text-green-600">3rd </span> annual python language
-          conference.
-        </h2>
-        <p>
-          For professionals, entrepreneurs, scientists and students in Tanzania,
-          organized by the tech-community to foster collaboration and
-          advancement of technology.
-        </p>
-        <button>Become A Sponsor</button>
-        <div className="flex">
-          <p>1ST - 4TH December 2021</p>
-          <a href="">VENUE → Institute of Accountancy Arusha</a>
-        </div>
-      </div>
-      <div></div>
+    <section>
+     
     </section>
   );
 }
--- components/Home/Home.js
+++ components/Home/Home.js
@@ -7,32 +7,27 @@ function Home(props) {
   return (
     <>
       <Head>
-        <title>Pycon Tanzania</title>
-        <meta
-          name="description"
-          content="The official website of Pycon Tanzania"
-        />
+          <title>Pycon Tanzania</title>
+          <meta name="description" content="The official website of Pycon Tanzania"/>
 
-        {/* OpenGraph Metatags */}
-        {/* <meta property="og:title" content="Albert Sigsbert Portfolio" />
-                <meta property="og:type" content="website" />
-                <meta property="og:url" content="https://albertsigsbert.netlify.app/" />
-                <meta property="og:description" content="Albert Sigsbert personal portfolio website" />
-                <meta property="og:image" content="https://albertsigsbert.netlify.app/og/home.png" /> */}
+          {/* OpenGraph Metatags */}
+          <meta property="og:title" content="Pycon Tanzania" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="http://next-pycon.vercel.app/" />
+          <meta property="og:description" content="The official website of Pycon Tanzania" />
+          {/* <meta property="og:image" content="https://albertsigsbert.netlify.app/og/home.png" /> */}
 
-        {/* Twitter Meta */}
-        {/* <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:site" content="@albert_sigsbert" />
-                <meta name="twitter:title" content="Albert Sigsbert Portfolio" />
-                <meta name="twitter:description" content="Albert Sigsbert personal portfolio website." />
-                <meta name="twitter:image" content="https://albertsigsbert.netlify.app/og/home.png" /> */}
+          {/* Twitter Meta */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:site" content="@PyconTanzania" />
+          <meta name="twitter:title" content="Pycon Tanzania" />
+          <meta name="twitter:description" content="The official website of Pycon Tanzania" />
+          {/* <meta name="twitter:image" content="https://albertsigsbert.netlify.app/og/home.png" /> */}
       </Head>
 
-      {/*          
-              <Hero/>
-                            */}
-
-            <h1 className="text-center text-4xl my-64">PyconTz</h1>
+               
+      <Hero/>
+                            
     </>
   );
 }
