30107c4fe54fc8a14b47e2133a4d9a8c244e5978
Albert <ishengomaalbert@gmail.com>
2022-11-22 16:51:31 +0300

    Implemented Report download for available years

--- components/UI/Header/NavLinks.js
+++ components/UI/Header/NavLinks.js
@@ -1,52 +1,127 @@
 import Link from "next/link";
-
-const navLinks = [
-  {
-    name: "about",
-    path: "/about",
-  },
-  {
-    name: "speak",
-    path: "/speaker",
-  },
-  {
-    name: "timetable",
-    path: "/timetable",
-  },
-  {
-    name: "sponsors",
-    path: "/sponsors",
-  },
-  {
-    name: "report",
-    path: "/report",
-  },
-  {
-    name: "code of conduct",
-    path: "/coc",
-  },
-];
+import { useRouter } from "next/router";
 
 function NavLinksLg(props) {
+  const router = useRouter();
+  const { eventYear } = router.query;
+  
+  const navLinks = [
+    {
+      name: "about",
+      path: "/about",
+    },
+    {
+      name: "speak",
+      path: "/speaker",
+    },
+    {
+      name: "timetable",
+      path: "/timetable",
+    },
+    {
+      name: "sponsors",
+      path: "/sponsors",
+    },
+    {
+      name: "report",
+      path: `/reports/report-${eventYear}.pdf`,
+    },
+    {
+      name: "code of conduct",
+      path: "/coc",
+    },
+  ];
+
+  let filteredLinks;
+  if (!eventYear || eventYear === "2020" || eventYear === "2019") {
+    filteredLinks = navLinks.filter((link) => link.name !== "report");
+  } else {
+    filteredLinks = navLinks;
+  }
+
   return (
     <nav className="hidden lg:flex gap-x-8 font-base font-semibold">
-      {navLinks.map((link) => (
-        <Link key={link.name} href={link.path}>
-          <a className="capitalize">{link.name}</a>
-        </Link>
-      ))}
+      {filteredLinks.map((link) => {
+        if (link.name === "report") {
+          return (
+            <a key={link.name} className="capitalize" href={link.path} download>
+              {link.name}
+            </a>
+          );
+        }
+
+        return (
+          <Link key={link.name} href={link.path}>
+            <a className="capitalize">{link.name}</a>
+          </Link>
+        );
+      })}
     </nav>
   );
 }
 
-function NavLinks({open, setOpen}) {
+function NavLinks({ open, setOpen }) {
+  const router = useRouter();
+  const { eventYear } = router.query;
+
+  const navLinks = [
+    {
+      name: "about",
+      path: "/about",
+    },
+    {
+      name: "speak",
+      path: "/speaker",
+    },
+    {
+      name: "timetable",
+      path: "/timetable",
+    },
+    {
+      name: "sponsors",
+      path: "/sponsors",
+    },
+    {
+      name: "report",
+      path: `/reports/report-${eventYear}.pdf`,
+    },
+    {
+      name: "code of conduct",
+      path: "/coc",
+    },
+  ];
+
+  let filteredLinks;
+ 
+  if (!eventYear || eventYear === "2020" || eventYear === "2019") {
+    filteredLinks = navLinks.filter((link) => link.name !== "report");
+  } else {
+    filteredLinks = navLinks;
+  }
+
   return (
     <nav className="mx-[6%] my-16 h-full w-1/2 flex flex-col space-y-8 font-base font-semibold">
-      {navLinks.map((link) => (
-        <Link key={link.name} href={link.path}>
-          <a className="capitalize" onClick={() => setOpen(!open)}>{link.name}</a>
-        </Link>
-      ))}
+      {filteredLinks.map((link) => {
+        if (link.name === "report") {
+          return (
+            <a
+              key={link.name}
+              href={link.path}
+              className="capitalize"
+              download
+              onClick={() => setOpen(!open)}
+            >
+              {link.name}
+            </a>
+          );
+        }
+
+        return (
+          <Link key={link.name} href={link.path} onClick={() => setOpen(!open)}>
+            <a className="capitalize">{link.name}</a>
+          </Link>
+        );
+      })}
     </nav>
   );
 }
---
+++ pages/report.js
@@ -0,0 +1,12 @@
+
+function Report(props) {
+    return (
+        <div>
+            <h1>Pycon Report: 2022</h1>
+        </div>
+    );
+}
+
+
+
+export default Report;
